73ade98dea89adc331cba2f5d91c5e4ce403ae75
ElioPeraza <Perazaelio@gmail.com>
2024-08-28 21:42:20 -0400

    " 														y
    Please enter the commit message for your changes. Lines startingdd
    add
    exit
    kick
    
    change
    
    ::
    														::#

--- src/js/app.js
+++ src/js/app.js
@@ -32,11 +32,11 @@ function render(variables = {}) {
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <img src="${variables.avatarURL  }" class="photo" />
+          <h1>${variables.name ?? ""} ${variables.lastNamename ?? ""}</h1>
+          <h2>${variables.role ?? ""} </h2>
+          <h3>${variables.city ?? ""}${variables.contry ?? ""}</h3>
+          <ul class=${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
@@ -89,3 +89,4 @@ window.onload = function() {
     });
   });
 };
+
