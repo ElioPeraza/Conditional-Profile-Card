43968cba08608ab9ba17fffe16a120aa6bd673c2
ElioPeraza <Perazaelio@gmail.com>
2024-08-29 20:25:46 -0400

    Second Update

--- src/js/app.js
+++ src/js/app.js
@@ -40,15 +40,17 @@ function render(variables = {}) {
   let city = variables.city === null ? " " : variables.city;
   let country = variables.country === null ? " " : variables.country;
 
+  let socialMediaClass = variables.socialMediaPosition === "position-right" ? "position-right" : "position-left";
+
  
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL }" class="photo" />
           <h1>${variables.name } ${variables.lastName}</h1>
-          <h2>${variables.role }</h2>
+          <h2>${variables.role }</h2> 
           <h3>${variables.city },${variables.country}</h3>
-          <ul class=${variables.socialMediaPosition}">
+          <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
